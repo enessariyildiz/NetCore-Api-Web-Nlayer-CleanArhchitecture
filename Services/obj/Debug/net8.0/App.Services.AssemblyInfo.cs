477430cdce2ab633a087d106186a98aa5cad16fe
Mithaverse <ens.sryldz@gmail.com>
2024-09-09 01:08:12 +0300

    Repositories-DbContext-Entity

--- Services/obj/Debug/net8.0/App.Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/App.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0c9999ecbf4b526819dabb42af9e4c10cd6295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7407319b9731e8f9f41c687f81e6e0bd4d074e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
