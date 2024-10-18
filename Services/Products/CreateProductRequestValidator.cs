3c76968c343915a29164f84a6e0a266b41de8bf1
Mithaverse <ens.sryldz@gmail.com>
2024-10-18 22:17:18 +0300

    Model validation-4

--- Services/Products/CreateProductRequestValidator.cs
+++ Services/Products/CreateProductRequestValidator.cs
@@ -1,14 +1,23 @@
-﻿using FluentValidation;
+﻿using App.Repositories.Products;
+using FluentValidation;
 
 namespace App.Services.Products
 {
     public class CreateProductRequestValidator :AbstractValidator<CreateProductRequest>
     {
+        private readonly IProductRepository _repository;
+
+        public CreateProductRequestValidator(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
         public CreateProductRequestValidator()
         {
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Ürün ismi gereklidir")
                 .Length(3, 10).WithMessage("Ürün ismi 3 ile 10 karakter arasında olmalıdır");
+                //.Must(MustUniqeProductName).WithMessage("Ürün ismi veritabanında bulunmaktadır");
 
             // Price Validation
             RuleFor(x => x.Price)
@@ -18,5 +27,12 @@ namespace App.Services.Products
             RuleFor(x => x.Stock)
                 .InclusiveBetween(1, 100).WithMessage("Stok adedi 1 ile 100 arasında olmalıdır");
         }
+
+        /*
+        private bool MustUniqeProductName(string name)
+        {
+            return !_repository.Where(x => x.Name == name).Any();
+        }
+        */
     }
 }
--- Services/obj/Debug/net8.0/App.Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/App.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69880f60088464bdf803cf1083c80c61ff01fdbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbde5b01572b7f8e6cec67dc868af82710ea7af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
