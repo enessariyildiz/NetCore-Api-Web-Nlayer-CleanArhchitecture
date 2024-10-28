94398ded2aef7a22f88f9ec72d3b1bdca8601447
Mithaverse <ens.sryldz@gmail.com>
2024-10-28 20:57:30 +0300

    Product service -Update

--- Services/Products/Create/CreateProductRequestValidator.cs
+++ Services/Products/Create/CreateProductRequestValidator.cs
@@ -23,6 +23,12 @@ namespace App.Services.Products.Create
             RuleFor(x => x.Price)
                 .GreaterThan(0).WithMessage("Ürün fiyatı 0'dan büyük olmalıdır");
 
+            // Price Validation
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("Ürün kategori değeri 0'dan büyük olmalıdır");
+
+
+
             // Stock InclusiveBetween Validation
             RuleFor(x => x.Stock)
                 .InclusiveBetween(1, 100).WithMessage("Stok adedi 1 ile 100 arasında olmalıdır");
