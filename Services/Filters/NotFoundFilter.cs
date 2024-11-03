896934f15116bddffacde82baeb3b2376ea32650
Mithaverse <ens.sryldz@gmail.com>
2024-11-03 22:08:21 +0300

    NotFoundFilter

---
+++ Services/Filters/NotFoundFilter.cs
@@ -0,0 +1,15 @@
+﻿using App.Repositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace App.Services.Filters
+{
+    public class NotFoundFilter<T>(IGenericRepository<T> genericRepository) : Attribute, IAsyncActionFilter where T : class
+    {
+
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+           
+        }
+    }
+}
